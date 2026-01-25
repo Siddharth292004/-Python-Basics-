9fcbd852153da5aadcd704770638f1ec0ebdbce7
Siddharth292004 <sidharth9509@gmail.com>
2026-01-25 19:45:27 +0530

    initial commit

---
+++ LIST/BASIC_LEVEL/insert.py
@@ -0,0 +1,10 @@
+#insert()
+
+#👉 Adds an item at a specific position
+
+numbers =  ["Ram","Shyam","Mohan","Sohan"]
+print(numbers)
+numbers.insert(2,"Ramesh")
+print(numbers)
+numbers.inser(0,"Suresh")
+
