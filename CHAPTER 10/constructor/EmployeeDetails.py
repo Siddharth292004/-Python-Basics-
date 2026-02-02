a6a79205809b1ac356eb65e86e08b4210f4f1210
Siddharth292004 <sidharth9509@gmail.com>
2026-02-02 19:47:11 +0530

    initial commit

---
+++ CHAPTER 10/constructor/EmployeeDetails.py
@@ -0,0 +1,22 @@
+"""
+Program to initialize two variables using constructor
+
+🔹 Hint:
+
+Pass two arguments
+
+Use self.variable
+"""
+
+
+class Employee:
+
+    def __init__(self,Name,salary):
+        self.Name = Name
+        self.salary= salary
+    
+    def display(self):
+        print(f"Employee Name: {self.Name}\nSalary: {self.salary}")
+
+obj = Employee("Siddharth",20000)
+obj.display()
