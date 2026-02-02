e59d91ce3aa8d8de34219da9e4183ee006be3a09
Siddharth292004 <sidharth9509@gmail.com>
2026-02-02 20:14:19 +0530

    initial commit

--- CHAPTER 10/chatGpt/classCreate.py
+++ CHAPTER 10/chatGpt/classCreate.py
@@ -1,12 +1,10 @@
-#1️⃣ Create a class and an object, then print a message.
-
-#🔹 Hint:
-
-#Create a class
-
-#Define one method
-
-#Call method using object
+"""
+1️⃣ Create a class and an object, then print a message.
+🔹 Hint:
+Create a class
+Define one method
+Call method using object
+"""
 
 
 class Greeting:
@@ -17,3 +15,15 @@ class Greeting:
 
 D1 = Greeting()
 D1.display()
+
+
+
+"""
+self points to the current object that is using the class.
+🔹 Why self is Used?
+Python uses self to:
+Connect object data with class methods
+Store values inside a specific object
+Tell Python “this variable belongs to this object”
+👉 Without self, Python cannot know where to store the data
+"""
