38c5ae1874ff47162ae84f18ceb5e550035512ec
Siddharth292004 <sidharth9509@gmail.com>
2026-02-06 17:42:56 +0530

    initial commit

---
+++ CHAPTER 11/GetterAndSetter/tempCodeRunnerFile.py
@@ -0,0 +1,19 @@
+class Student:
+    def __init__(self, name):
+        self._name = name   # private variable
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self,name):
+        if not name.strip():
+            raise ValueError("Name cannot be empty!")
+        self._name = name
+
+
+s = Student("Siddharth")
+print(s.name)
+s.name = "Rahul"
+print(s.name)
