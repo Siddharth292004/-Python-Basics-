f4f48ded7557e37117d7f5223742fae874d20a95
Siddharth292004 <sidharth9509@gmail.com>
2026-02-06 08:01:49 +0530

    inital commit

---
+++ Questions/Basic_level/program_02.py
@@ -0,0 +1,18 @@
+"""
+Docstring for Questions.Basic_level.program_02
+2.Write a Python program to find out what version of Python you are using.
+"""
+
+import sys
+
+print("Python version")
+
+print(sys.version)
+
+print("version info.")
+
+print(sys.version_info)
+
+print(sys.version_info[0])
+print(sys.version_info[1])
+print(sys.version_info[2])
