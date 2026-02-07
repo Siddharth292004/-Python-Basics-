319186414088485a903da118a70deb63abdc9a8e
Siddharth292004 <sidharth9509@gmail.com>
2026-02-07 14:27:23 +0530

    initial commit

---
+++ CHAPTER 12/walrus/tempCodeRunnerFile.py
@@ -0,0 +1,12 @@
+"""
+What is the walrus operator?
+The walrus operator is :=
+It assigns a value to a variable AND returns that value at the same time.
+👉 Introduced in Python 3.8
+"""
+
+# simple term: you can assign a value to a variable and use it in the same expression
+# without the walrus operator, you would have to do this in two steps:
+
+if (n := len("walrus operator")) > 10:
+    print(f"The string is too long with {n} characters.")
