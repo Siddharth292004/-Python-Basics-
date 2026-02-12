5f634b09f1f84ea7036a4bd5225910d69744175f
Siddharth292004 <sidharth9509@gmail.com>
2026-02-12 19:00:03 +0530

    initial commit

--- Numpy/Paratice Numpy/arrayInfo.py
+++ Numpy/Paratice Numpy/arrayInfo.py
@@ -7,3 +7,6 @@ print(arr1.shape)
 print(arr1.size)
 print(arr1.dtype)
 print(np.sum(arr))
+
+arr3 = np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(arr3.shape)
---
+++ Numpy/Paratice Numpy/makArray.py
@@ -0,0 +1,62 @@
+import numpy as np
+import random
+
+a = np.array([1,2,3])
+b = np.array([4,5,6])
+
+result = a + b 
+print(result)
+
+
+arr = np.array([10,20,30,40,50])
+print(arr)
+
+
+matrix = [[1,2,3],[4,5,6]]
+print(matrix)
+
+
+arr1= np.array([1,2,3])
+
+print(arr1 * 3)
+print(arr1 * 4)
+print(arr1 + 3)
+
+
+arr2 = np.array([10,20,30,40,50])
+print(arr2)
+
+arr3 = np.array([[1,2,3],
+                [4,5,6],
+                [7,8,9]])
+
+arr4 = np.array([[[1,2],[3,4],
+                   [5,6], [7,8]]])
+
+print(arr4)
+
+arr5 = np.array([3,4,5,6,7])
+print(arr5)
+
+arr6 = np.arange(1,20,2)
+print(arr6)
+
+arr7 = np.zeros(3)
+print(arr7)
+
+arr8 = np.ones(4)
+
+arr9 = np.arange(1,50,3)
+
+print(arr9)
+
+arr10 = np.random.rand(5)
+print(arr10)
+
+
+arr11 = np.eye(4)
+print(arr11)
+
+arr12 = np.array([1,2,3,4,5])
+print(arr12)
+
