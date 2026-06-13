ad114d2c6c22cea1db2b132a978f0d9011f0d113
Siddharth Singh <your-email@example.com>
2026-06-13 12:50:56 +0530

    initial commit

---
+++ paratice/resetPratice/01_pratice.py
@@ -0,0 +1,57 @@
+print("Hello World")
+
+name = "siddharth"
+age = 22
+
+print(name)
+
+student_program= "siddhart Singh"
+age1 = 24
+_marks = 90
+
+
+age = 34
+print(type(age))
+
+price = 99.99
+print(type(price))
+
+name = "siddhart"
+print(type(name))
+
+is_student = True
+print(is_student)
+print(type(is_student))
+
+name = input("Enter your name: ")
+print("hello ",name)
+
+# Type Casting
+# Type casting converts one data type into another
+
+age = "22"
+new_age = int(age)
+print(type(age))
+print(new_age)
+print(type(new_age))
+
+num = 100
+
+new_str = str(num)
+print(new_str)
+print(type(new_str))
+
+ages = int(input("Enter age: "))
+print(ages + 80)
+
+# Operator
+
+a = 10
+b  = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a// b)
+print(a **b)
