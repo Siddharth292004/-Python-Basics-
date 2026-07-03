afa3cb497698a128ffa5de5848d13c521d681d79
Siddharth Singh <your-email@example.com>
2026-07-03 20:30:07 +0530

    Intial commit

---
+++ PYTHON UDEMY/03_conditions/snak_suggestion.py
@@ -0,0 +1,18 @@
+snack = input("Enter your preferred snack: ").lower()
+
+if snack == "cookies" or snack =="samosa":
+    print(f"Greeat choice ! we'll server you {snack}")
+else:
+    print(f"Sorry, we only serve cookies or samosa")
+
+
+
+"""
+snack = input("Enter your preferred snack: ").lower()
+
+if snack in ["cookies", "samosa"]:
+    print(f"Great choice! We'll serve you {snack}.")
+else:
+    print("Sorry, we only serve cookies or samosa.")
+
+"""
