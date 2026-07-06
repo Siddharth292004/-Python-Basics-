8c07923fe34c090eb18189e0658511d4163fc9a9
Siddharth Singh <your-email@example.com>
2026-07-06 10:04:15 +0530

    Intial commit

---
+++ PYTHON UDEMY/05_function/03_hiding.py
@@ -0,0 +1,14 @@
+def get_input():
+    print("Getting user input")
+def validate_input():
+    print("Validating the user info")
+
+def save_to_db():
+    print("saving to databse")
+
+def resgister_user():
+    get_input()
+    validate_input()
+    save_to_db()
+
+resgister_user()
