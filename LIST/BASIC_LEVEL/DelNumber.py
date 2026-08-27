b94b5ca12aaccb13be349cb72117dc92d9903c37
Siddharth Singh <your-email@example.com>
2026-08-27 19:58:14 +0530

    inital commit

--- LIST/BASIC_LEVEL/DelNumber.py
+++ LIST/BASIC_LEVEL/DelNumber.py
@@ -8,4 +8,15 @@ print(numbers)
 num = [1,2,3,4,5,6,7,8,9]
 
 del num
-print(num)  # This will cause an error because numbers no longer exist
+# print(num)  # This will cause an error because numbers no longer exist
+
+collection = ['Apple','Banana','cherry','date']
+
+del collection[-1]
+print(collection)
+
+del collection[0:3]
+print(collection)
+
+del collection
+# print(collection)  # This give also error and say collection is not define
